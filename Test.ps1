5ee6daed344fd8aa327ef8d66717616cdc7c9a34
jimddavis <jimddavis_00@yahoo.com>
2021-11-27 20:38:06 -0500

    Just some clean up

--- Test.ps1
+++ Test.ps1
@@ -7,11 +7,28 @@ Clear-Host
 
 $img = [ImageDownload]::new()
 
+$img.AddDownloadType([NasaAPOD]::new())
+$img.AddDownloadType([EarthObservatoryPOD]::new())
+$img.AddDownloadType([OutdoorPhotographer]::new([OutdoorPhotoFeedEnum]::FeaturedStories), 'C:\Users\JimD\Pictures\OutdoorPhotographer')
+$img.AddDownloadType([OutdoorPhotographer]::new([OutdoorPhotoFeedEnum]::FavoritePlaces), 'C:\Users\JimD\Pictures\OutdoorPhotographer')
+$img.AddDownloadType([OutdoorPhotographer]::new([OutdoorPhotoFeedEnum]::Travel), 'C:\Users\JimD\Pictures\OutdoorPhotographer')
+
+
+if ($img.TestInternetConnection()) {
+  $img.Invoke()
+
+  #$img.Results | Format-list
+  $img.Results | Out-File  'c:\temp\ImgDownload.log' -Append
+
+}
+
+
+
+
+<#
+
 #$img.AddDownloadType([NasaAPOD]::new('C:\Users\JimD\Pictures\Nasa'))
 #$img.AddDownloadType([EarthObservatoryPOD]::new('C:\Users\JimD\Pictures\EarthObservatory'))
-
-#$test = [TestEnum]::new([OutdoorPhotoFeedEnum]::FavoritePlaces)
-
 $op = [OutdoorPhotographer]::new([OutdoorPhotoFeedEnum]::FeaturedStories)
 $op.GetEnumVal()
 $op.DownloadFolder = 'C:\Users\JimD\Pictures\OutdoorPhotographer'
@@ -19,16 +36,7 @@ $op.TempFolder = 'C:\temp\OutdoorPhotography'
 
 $img.AddDownloadType($op)
 
-#$img.AddDownloadType([OutdoorPhotographer]::new('C:\Users\JimD\Pictures\OutdoorPhotographer'))
-
-
-if ($img.TestInternetConnection()) {
-  $img.Invoke()
-
-  $img.Results | Format-list
-  $img.Results | Out-File  'c:\temp\ImgDownload.log' -Append
-
-}
+#$img.AddDownloadType([OutdoorPhotographer]::new('C:\Users\JimD\Pictures\OutdoorPhotographer'))#>
 
 
 
